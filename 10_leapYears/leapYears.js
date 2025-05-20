7fa435b24e66254d7514e2d82762a57cffda6b73
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-20 15:11:20 -0600

    Complete leapYears.js exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+const isDivFour = year % 4 == 0;
+const isCentury = year % 100 == 0;
+const isDivFourHundred = year % 400 == 0;
 
+if (isDivFour && (!isCentury || isDivFourHundred)) {
+    return true
+} else {
+    return false
+}
 };
 
 // Do not edit below this line
