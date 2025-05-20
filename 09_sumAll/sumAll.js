0ebe5e4cb32eba8bd327528f2565c3830a48c882
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-20 14:46:19 -0600

    Order numbers correctly and add them using loop

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(lowNum, highNum) {
+    // swap numbers into correct position
+    if (lowNum > highNum) [lowNum, highNum] = [highNum, lowNum];
+    let sumTotal = 0
 
+    // add numbers using for loop increment
+    for (i = lowNum; i < highNum + 1; i++) {
+        sumTotal += i;
+    }
+
+    return sumTotal;
 };
 
 // Do not edit below this line
