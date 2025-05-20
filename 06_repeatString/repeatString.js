f6a8480539d71a2e9a053af8fb958ebbb58a5e07
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-20 13:26:58 -0600

    Complete repeatString.js exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(message, count) {
+    let returnMsg = ""
 
+    // check boundaries
+    if (count < 0) {
+        return "ERROR"
+    }
+
+    // repeat message
+    for (i = 0; i < count; i ++) {
+        returnMsg = returnMsg + message
+    }
+
+    return returnMsg
 };
 
 // Do not edit below this line
