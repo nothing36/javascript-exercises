118ec337544b1747f5fc1cd19681e267d9a2bfe1
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-26 10:36:44 -0600

    Complete fibonacci.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 0) {return 0}
+    if (num < 0) {return "OOPS"}
+    let fibArr = [0, 1]
 
+    for (let i = 2; i <= num; i++) {
+        fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+    }
+    
+    return fibArr.pop()
 };
 
 // Do not edit below this line
