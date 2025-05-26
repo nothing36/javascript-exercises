035367a71984b5064b18a88c15b8ed115a530acf
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-26 10:29:46 -0600

    Complete palindromes.js exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanWord = word
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
 
+    backwords = cleanWord.split("").reverse().join("")
+    if (backwords == cleanWord) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
