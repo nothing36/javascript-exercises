a72f9ba4fbcd69b7ebb9ead4e853fcf697586421
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-26 12:41:53 -0600

    Complete findTheOldest.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    let oldestPerson = null
+    let oldestAge = null
 
+    peopleArr.forEach(person => {
+        let age;
+        if (!person.yearOfDeath) {
+            age = new Date().getFullYear() - person.yearOfBirth
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth
+        }
+
+        if (age > oldestAge || oldestPerson == null) {
+            oldestPerson = person
+            oldestAge = age
+        }
+    });
+
+    return oldestPerson
 };
 
 // Do not edit below this line
